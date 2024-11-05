c700b3d63a7b238842de3abc2c36da03021ebb66
Igor Tomelero de Almeida <inazumaseleven04@gmail.com>
2024-11-05 15:52:44 -0300

    primeiro

---
+++ src/main/java/com/compass/spring_course_1/SpringCourse1Application.java
@@ -0,0 +1,13 @@
+package com.compass.spring_course_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCourse1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCourse1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/compass/spring_course_1/SpringCourse1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.compass.spring_course_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCourse1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
